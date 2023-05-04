a3ed31bab798747443a628092b04737735ab4e4d
AlexProstolucky <alexprostolucky@gmail.com>
2023-05-04 22:29:35 +0300

    Добавьте файлы проекта.

---
+++ Password_check/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string patern = @"^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[*#+@])(?!.*\s).{4,6}$";
+        Regex rg = new Regex(patern);
+        Console.WriteLine("Write your passwords:\n");
+        string str = Console.ReadLine();
+        if (rg.IsMatch(str)) 
+        {
+            Console.WriteLine("Correct!");
+        }
+        else Console.WriteLine("Uncorrect!");
+        Console.Write("\b");
+    }
+}
